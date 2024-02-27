012373347bb9d77e974e24176367307d70551ce5
claus <claus@192.168.1.134>
2024-02-27 21:56:31 +0100

    Primer commit con la carga inicial del proyecto

--- src/tareacd4/Main.java
+++ src/tareacd4/Main.java
@@ -1,23 +1,23 @@
 package tareacd4;
 
+import banco.CtaCorriente;
 import java.util.Scanner;
 
 public class Main {
-
+   
     public static void main(String[] args) {
-        CC miCuenta;
+        CtaCorriente miCuenta;
         double saldoActual;
 
-        miCuenta = new CC("Koldo García Ábalos","0001-2345-07-1234567890",5000,0);
-        saldoActual = miCuenta.estado();
-        System.out.println("El saldo inicial es de "+ saldoActual +" euros");
+        miCuenta = new CtaCorriente("Koldo García Ábalos","0001-2345-07-1234567890",5000,0);
+    }
+    public static void operativa_cuenta(CtaCorriente cuenta) {
 
         try {
             Scanner entrada = new Scanner(System.in);
             System.out.println("¿Cuánto desea retirar?");
             double importe = entrada.nextDouble();
-            miCuenta.retirar(importe);
-            System.out.println("El saldo actual de su cuenta es "+miCuenta.estado()+" euros");
+            cuenta.retirar(importe);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
@@ -25,10 +25,10 @@ public class Main {
             Scanner entrada = new Scanner(System.in);
             System.out.println("¿Cuánto desea ingresar en su cuenta?");
             double importe = entrada.nextDouble();
-            miCuenta.ingresar(importe);
-            System.out.println("El saldo actual de su cuenta es "+miCuenta.estado()+" euros");
+            cuenta.ingresar(importe);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
     }
+   
 }
